81b855863d97871c775a3f565e03d9f46f349a3f
masonasons <darkflier01@gmail.com>
2021-07-14 11:09:02 -0500

    updates changelog and bumps version

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-nuitka --windows-company-name=Mellaquin --windows-product-name=Quinter --windows-file-version=0.59 --windows-product-version=0.59 --windows-file-description=Quinter --standalone --python-flag=no_site --include-data-file=keymap.keymap=keymap.keymap --windows-disable-console --output-dir=c:\tempbuild --remove-output quinter.pyw
+nuitka --windows-company-name=Mellaquin --windows-product-name=Quinter --windows-file-version=0.60 --windows-product-version=0.60 --windows-file-description=Quinter --standalone --python-flag=no_site --include-data-file=keymap.keymap=keymap.keymap --windows-disable-console --output-dir=c:\tempbuild --remove-output quinter.pyw

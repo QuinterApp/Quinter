91c20149bffc22b357ed7ee2fd2cd87bddb2231f
masonasons <darkflier01@gmail.com>
2021-08-24 14:35:40 -0500

    fix merge conflict

--- application.py
+++ application.py
@@ -1,10 +1,4 @@
 shortname="quinter"
-<<<<<<< HEAD
-name="Quinter"
-version="0.67"
-author="Quin and Melody"
-=======
 name = "Quinter"
 version = "0.67"
 author = "Quin and Melody"
->>>>>>> 7278a0788ee2059ec1d35f21b1232792ffe60a9c
